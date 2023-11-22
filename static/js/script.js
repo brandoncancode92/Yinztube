3764d085b5572e5e9f9a9c46145ed7ea2d44f6b5
Brandon Colangelo <brandoncancode@gmail.com>
2023-11-22 09:27:16 -0500

    Working on the add transitions. There is a problem after the first full cycle of the time in between transitions going from 15 seconds down to every second.

--- static/js/script.js
+++ static/js/script.js
@@ -37,3 +37,51 @@ burgerIcon.addEventListener('click', () => {
         }
     }
 });
+
+const ads = document.querySelectorAll('#ads > div'); // Select all ad elements
+let currentAdIndex = 0; // Initialize the current ad index
+
+// Initialize the ad rotation loop
+const adRotationInterval = setInterval(() => {
+    // Fade out the current ad
+    ads[currentAdIndex].classList.remove('ad-enter');
+    ads[currentAdIndex].classList.add('ad-leave'); // Apply the 'ad-leave' animation class
+
+    // Handle the animationend event for the last ad (ad-3) separately
+    if (currentAdIndex === ads.length - 1) {
+        ads[currentAdIndex].addEventListener('animationend', () => {
+            // Update the display property after the fade-out animation completes
+            ads[currentAdIndex].style.display = 'none';
+
+            // Reset the current ad index to start the loop again from the first ad
+            currentAdIndex = 0;
+
+            // Show the first ad and apply the 'ad-enter' animation class
+            ads[currentAdIndex].style.display = 'block';
+            ads[currentAdIndex].classList.add('ad-enter');
+        });
+    } else {
+        // Handle animationend events for other ads (ad-1, ad-2)
+        ads[currentAdIndex].addEventListener('animationend', () => {
+            // Update the display property after the fade-out animation completes
+            ads[currentAdIndex].style.display = 'none';
+
+            // Update the current ad index
+            currentAdIndex = (currentAdIndex + 1) % ads.length;
+
+            // Show the next ad and apply the 'ad-enter' animation class
+            ads[currentAdIndex].style.display = 'block';
+            ads[currentAdIndex].classList.add('ad-enter');
+
+            // Remove the event listener to prevent multiple callbacks
+            ads[currentAdIndex].removeEventListener('animationend', this);
+        });
+    }
+}, 15000);
+
+// Initially show the first ad and apply the 'ad-enter' animation class
+ads[currentAdIndex].style.display = 'block';
+ads[currentAdIndex].classList.add('ad-enter');
+
+// Keep the ad rotation interval active
+
